b03cf03784b66b1b4cac8e599d5c030fb2fb5b92
sachieko <kirazetsubou@gmail.com>
2022-08-08 15:38:29 -0600

    Passing url vars to template

--- express_server.js
+++ express_server.js
@@ -8,7 +8,8 @@ app.set('view engine', 'ejs');
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
+  "b4k4m1": "http://www.example.com"
 };
 
 app.get('/', (req, res) => {
@@ -19,6 +20,10 @@ app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>Waarudo</b></body></html>\n');
 });
 
+app.get('/urls', (req, res) => {
+  const templateVar = { urls: urlDatabase };
+  res.render('urls_index', templateVar);
+})
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
