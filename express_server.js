b00c5854537371bd98fc7fbc9978e696a661a1f6
sachieko <kirazetsubou@gmail.com>
2022-08-09 15:50:27 -0600

    Reformatted comments

--- express_server.js
+++ express_server.js
@@ -16,71 +16,75 @@ const generateRandomString = function() {
   return string;
 };
 
-// middleware
+//
+// Middleware
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan(':method :url :status'));
+app.use(morgan('dev')); // Dev middleware for debugging
 app.use('/images', express.static('images'));
 
+//
+// Create database from file
 const urlDatabase = JSON.parse(fs.readFileSync('urlDatabase.json'));
 
+//
 // GET /
 app.get('/', (req, res) => {
   res.send("Hello!");
 });
+//
 // GET hello
 app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>Waarudo</b></body></html>\n');
 });
-
+//
 // GET urls
 app.get('/urls', (req, res) => {
   const templateVar = { urls: urlDatabase };
   res.render('urls_index', templateVar);
 });
-
-
+//
 // GET urls/new
 app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 });
-
+//
 // POST urls
 app.post('/urls', (req, res) => {
   let randomString = generateRandomString();
   urlDatabase[randomString] = req.body.longURL;
-  res.redirect(`/urls/${randomString}`);
+
   fs.writeFile('urlDatabase.json', JSON.stringify(urlDatabase), (err) => {
     if (err) {
       throw err;
     }
     console.log("Server updated after post");
+    res.redirect(`/urls/${randomString}`);
   });
 });
-
+//
 // GET urls/:id
 app.get('/urls/:id', (req, res) => {
   const templateVar = { id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render('urls_show', templateVar);
 });
-
+//
 // GET u/:id in database
 app.get('/u/:id', (req, res) => {
   const templateVar = { id: req.params.id, longURL: urlDatabase[req.params.id]};
   urlDatabase[templateVar.id] ? res.redirect(templateVar.longURL) : res.redirect('/418');
 });
-
+//
 // GET urls.json
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
-
-
+//
 // GET catchall
 app.get('*', (req, res) => {
   res.status(418).render('418');
 });
-
+// Start server to listen
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
