81129fb7230e6299c265a7177af1434974c59c1a
sachieko <kirazetsubou@gmail.com>
2022-08-08 20:31:25 -0600

    Added view for new urls and for pages that don't exist

--- express_server.js
+++ express_server.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const morgan = require('morgan');
+
+const generateRandomString = function() {
+  let string = '';
+  const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  const charLength = characters.length;
+  // This sets how long all newly generated strings will be for urls. Change if you want longer/shorter
+  const length = 6;
+  for (let i = 0; i < length; i++) {
+    string += characters.charAt(Math.floor(Math.random() * charLength));
+  }
+  return string;
+};
 
 // middleware
 app.set('view engine', 'ejs');
-
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan(':method :url :status'));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -25,6 +39,17 @@ app.get('/urls', (req, res) => {
   const templateVar = { urls: urlDatabase };
   res.render('urls_index', templateVar);
 });
+// POST urls
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  let randomString = generateRandomString();
+  res.send(`New url posted to: /${randomString}`);
+});
+
+// GET urls/new
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
 
 // GET urls/:id
 app.get('/urls/:id', (req, res) => {
@@ -34,6 +59,11 @@ app.get('/urls/:id', (req, res) => {
 // GET urls.json
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
+});
+
+// GET catchall
+app.get('*', (req, res) => {
+  res.status(418).render('418');
 });
 
 app.listen(PORT, () => {
