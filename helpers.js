3d90aacab4a9375aaeb94a3161981a0b8912738f
sachieko <arashikabane@gmail.com>
2022-08-11 18:38:56 -0600

    Changed userClass filepath

--- helpers.js
+++ helpers.js
@@ -1,6 +1,6 @@
 const getUserByEmail = require('./getUserByEmail');
 const generateRandomString = require('./generateRandomString');
-const User = require('./user'); // Used to register new users
+const User = require('./userClass'); // Used to register new users
 const Url = require('./urlClass'); // Used to register new URLs
 const updateDatabase = require('./updateDatabase'); // Used to update database json files.
 const urlsForUser = require("./urlsForUser"); // Filters object to only have keys meeting a string input
