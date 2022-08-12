2ed2e2878f07ba10b2bed94948061da8767e6d20
sachieko <arashikabane@gmail.com>
2022-08-11 21:07:26 -0600

    Updated comments

--- helpers/updateDatabase.js
+++ helpers/updateDatabase.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 // updateDatabase takes in the path to your json database as a string, the dataObject, a callback, and an optional message.
-// The callback is intended for res.redirects or res.renders
-// It will console log to your server's console whatever the message is when it is called. Helpful for debugging.
+// The callback is intended for res.redirects or res.renders so a user only gets logged in once they're in the database etc.
+// If you give it the optional message, it will console log to your server's console whatever the message is when it is called for debugging.
 const updateDatabase = function(dbPath, dataObject, callback, message) {
   fs.writeFile(dbPath, JSON.stringify(dataObject), (err) => {
     if (err) {
