2936b4d5e4aecf9e0d9464c28fb724f10ad404a4
sachieko <arashikabane@gmail.com>
2022-08-11 19:57:54 -0600

    Added descriptions of each helper function/class

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,8 +1,8 @@
-const getUserByEmail = require('./getUserByEmail');
-const generateRandomString = require('./generateRandomString');
-const User = require('./userClass'); // Used to register new users
-const Url = require('./urlClass'); // Used to register new URLs
-const updateDatabase = require('./updateDatabase'); // Used to update database json files.
-const urlsForUser = require("./urlsForUser"); // Filters object to only have keys meeting a string input
+const getUserByEmail = require('./getUserByEmail'); // Use: getUserByEmail(databaseObject, emailString) => returns userObject
+const generateRandomString = require('./generateRandomString'); // Use: generateRandomString(number) =>  creates random alphanumeric string of specified length
+const User = require('./userClass'); // Use: new User(uID, email, passwordHash) => creates user obj with { uid: uID, email: email, password: passwordHash }
+const Url = require('./urlClass'); // Use: new Url(longURL, userID) => creates url obj with { longURL: longURL, userID: userID, useCount: 0 }
+const updateDatabase = require('./updateDatabase'); // Params: (dbPath, dataObject, callback, message) async writes dataObject to JSON file at dbPath before doing callback. Message is optional to console log for debugging
+const urlsForUser = require("./urlsForUser"); // Use: urlsForUser(userid, urlData) => returns object with only the urls that match from the urlData object
 
 module.exports = { getUserByEmail, generateRandomString, User, Url, updateDatabase, urlsForUser };
